fa75c189eadde0a9e46b0222e493e9e0c47e639a
Marcel Gaweda <github@mgaweda.de>
2025-02-06 15:00:13 +0100

    adapted init to the plotting color palettes

--- scripts/notebook_utils/__init__.py
+++ scripts/notebook_utils/__init__.py
@@ -3,6 +3,10 @@ from .plotting import (
     plot_regression_results,
     plot_synthetic_data,
     plot_true_predicted_comparison,
+    REGRESSION_COLORPALETTE,
+    TRUEPARAM_COLORPALETTE,
+    PREDPARAM_COLORPALETTE,
+    ELBO_COLOR
 )
 
 
@@ -11,4 +15,8 @@ __all__ = [
     "plot_regression_results",
     "plot_synthetic_data",
     "plot_true_predicted_comparison",
+    "REGRESSION_COLORPALETTE",
+    "TRUEPARAM_COLORPALETTE",
+    "PREDPARAM_COLORPALETTE",
+    "ELBO_COLOR"
 ]
