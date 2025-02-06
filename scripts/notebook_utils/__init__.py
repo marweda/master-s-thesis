8bdb8af65fa8d624ff0e35c29f418e39b9af0886
Marcel Gaweda <github@mgaweda.de>
2025-02-06 14:46:39 +0100

    created init for plotting.py

---
+++ scripts/notebook_utils/__init__.py
@@ -0,0 +1,14 @@
+from .plotting import (
+    plot_elbo,
+    plot_regression_results,
+    plot_synthetic_data,
+    plot_true_predicted_comparison,
+)
+
+
+__all__ = [
+    "plot_elbo",
+    "plot_regression_results",
+    "plot_synthetic_data",
+    "plot_true_predicted_comparison",
+]
