fe32d363d69fa7f642ff7e87106c2a7710fc1f82
Marcel Gaweda <github@mgaweda.de>
2025-02-10 17:58:21 +0100

    added CustomALD

--- scripts/vigamlss/__init__.py
+++ scripts/vigamlss/__init__.py
@@ -1,5 +1,6 @@
 from .data_preperation import DataPreparator
 from .utils.transformations import TransformationFunctions
+from .utils.custom_tf_distributions import CustomALD
 from .model.distributions import (
     Normal,
     Gamma,
@@ -26,6 +27,7 @@ __all__ = [
     "GEV",
     "GEVFixedShape",
     "ALD",
+    "CustomALD",
     "HalfCauchy",
     "VariationalDistribution",
     "FullCovarianceNormal",
